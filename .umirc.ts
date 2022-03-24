7d41a452d3c9f944f78f589bf58e91644e6c3135
shengjunpeng <shengjunpeng@yiwise.com>
2022-03-24 16:17:10 +0800

    feat: 使用ejs给dom添加一个版本信息

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,13 @@ import routes from './config/routes'
 import proxy from './config/proxy'
 import theme from './config/theme'
 import zhCN from 'antd/lib/locale-provider/zh_CN'
+import dayjs from 'dayjs'
 
 const path = require('path')
 const isDev = process.env.NODE_ENV === 'development'
+
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
+const gitRevisionPlugin = new GitRevisionPlugin()
 
 export default defineConfig({
   title: 'RPA',
@@ -14,6 +18,17 @@ export default defineConfig({
   history: {
     //hash路由
     type: 'hash'
+  },
+  define: {
+    __DEV__: process.env.NODE_ENV === 'development',
+    __ONLINE_ENV__: process.env.ONLINE_ENV, // daily, prepub, prod
+    __DAILY_ONLINE_ENV__: process.env.ONLINE_ENV === 'daily', // 日常环境
+    __PREPUB_ONLINE_ENV__: process.env.ONLINE_ENV === 'prepub', // 预发环境
+    __PROD_ONLINE_ENV__: process.env.NODE_ENV === 'prod', // 正式环境
+    publishDate: `${dayjs().format('YYYY-MM-DD HH:mm:ss')}`,
+    gitVersion: gitRevisionPlugin.version().split('-')[0],
+    gitCommit: gitRevisionPlugin.commithash(),
+    gitBranch: gitRevisionPlugin.branch(),
   },
   dynamicImport: {
     loading: '@/components/loading'
